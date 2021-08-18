75b56ec4fe797c4d79b2f79d0618c020781e6849
Russ Savage <russ@influxdata.com>
2021-08-18 11:32:49 -0400

    chore: adding docs to code

--- lambda_function.py
+++ lambda_function.py
@@ -4,16 +4,28 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.domain.write_precision import WritePrecision
 
 def lambda_handler(event, context):
+    # make sure the bucket you set here is already created in your InfluxDB instance
     bucket = "my-bucket"
+    # This specifies the precision of your timestamp. For example, standard unix
+    # timestamps have a precision of second (WritePrecision.S)
+    # NanoSecond is the default for InfluxDB
+    timestamp_precision = WritePrecision.NS
+
+    # You can use this to build a data point from your event.
+    # For more information about measurements, tags, and fields, check out
+    # https://docs.influxdata.com/influxdb/cloud/reference/key-concepts/data-elements/
     point = {
         "measurement" : "",
-        "tags" : [],
-        "fields" : [],
+        "tags" : {},
+        "fields" : {},
         "timestamp" : 0
     }
 
+    record = Point.from_dict(point, write_precision=timestamp_precision)
+    records_to_send = [record]
+
     write_api = _get_write_api()
-    results = write_api.write(bucket=bucket, record=[Point.from_dict(point, write_precision=WritePrecision.NS)])
+    results = write_api.write(bucket=bucket, record=records_to_send)
     
     return {
         'statusCode': 200,
